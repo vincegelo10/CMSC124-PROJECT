5a12959b4a8d42353de18fcdeeabb7199f77a091
vincegelobuala <vdbuala@up.edu.ph>
2021-12-06 15:56:37 +0800

    added other lexemes

--- lexicalanalysis.py
+++ lexicalanalysis.py
@@ -1,3 +1,4 @@
+from variables import *
 import re 
 
 def lexicalAnalysis():
@@ -22,15 +23,20 @@ def lexicalAnalysis():
 	NO_WAI = ""
 	IM_IN_YR = ""
 	IM_OUTTA_YR = ""
+	YARN = ""
+
 	singlelinecomment = False
 	multilinecomment = False
+	isString = False
+
+	if_else = False
+	switch_case = False
 	
 	fileHandle = open("sample.txt",'r')
 	for line in fileHandle:
 		line = line.split()
 		for token in line:
-
-			if not singlelinecomment and not multilinecomment:
+			if not singlelinecomment and not multilinecomment and not isString:
 				if re.search("^HAI$",token):
 					print(token, "-Code delimeter")
 				elif re.search("^KTHXBYE$", token):
@@ -53,6 +59,73 @@ def lexicalAnalysis():
 					print(token,"-typecast")
 				elif re.search("^VISIBLE$", token):
 					print(token,"-print keyword")
+				elif re.search("^MEBBE$", token):
+					print(token,"-else-if condition")
+				elif re.search("^OIC$", token):					#check for if-else or loops
+					if if_else:
+						print(token,"-if-then delimeter")
+						if_else = False
+					elif switch_case:
+						print(token,"-loop delimeter")
+						switch_case = False
+				elif re.search("^WTF?$", token):
+					print(token,"-switch-case delimeter")
+					switch_case = True
+				elif re.search("^OMG$", token):
+					print(token,"-switch-case condition")
+				elif re.search("^OMGWTF$", token):
+					print(token,"-switch-case default condition")
+				elif re.search("^UPPIN$",token):
+					print(token,"-loop-increment")
+				elif re.search("^NERFIN$",token):
+					print(token,"-loop-decrement")
+				elif re.search("^TIL$",token):
+					print(token,"-loop-condition")
+				elif re.search("^WILE$",token):
+					print(token,"-loop-condition")
+				elif re.search("^(WIN|FAIL)$", token):
+					print(token, "TROOF")
+				elif re.search("^(TROOF|NOOB|NUMBR|NUMBAR|YARN|TYPE)$",token):
+					print(token,"TYPE LITERAL")
+				elif re.search("^IM$",token):
+					IM_IN_YR = IM_IN_YR + "IM"
+					IM_OUTTA_YR = IM_OUTTA_YR + "IM"
+				elif re.search("^IN$",token):
+					if IM_IN_YR:
+						IM_IN_YR = IM_IN_YR + " IN"
+						IM_OUTTA_YR = ""
+				elif re.search("^YR$",token):
+					if IM_IN_YR:
+						IM_IN_YR = IM_IN_YR + " YR"
+					elif IM_OUTTA_YR:
+						IM_OUTTA_YR = IM_OUTTA_YR + " YR"
+				elif re.search("^OUTTA$",token):
+					if IM_OUTTA_YR:
+						IM_OUTTA_YR = IM_OUTTA_YR + " OUTTA"
+						IM_IN_YR = ""
+				elif re.search("^O$",token):
+					O_RLY = O_RLY + "O"
+				elif re.search("^RLY?$",token):
+					if O_RLY:
+						O_RLY = O_RLY + " RLY?"
+						print(O_RLY,"if-then delimeter")
+						O_RLY = ""
+						if_else = True
+				elif re.search("^YA$",token):
+					YA_RLY = YA_RLY + "YA"
+				elif re.search("^RLY$",token):
+					if YA_RLY:
+						YA_RLY = YA_RLY + " RLY"
+						print(YA_RLY,"-if-statement")
+						YA_RLY = ""
+				elif re.search("^NO$",token):
+					NO_WAI = NO_WAI + "NO"
+				elif re.search("^WAI$",token):
+					if NO_WAI:
+						NO_WAI = NO_WAI + " WAI"
+						print(NO_WAI,"-else-statement")
+						NO_WAI = ""
+
 				elif re.search("^I$", token):
 					I_HAS_A = token
 				elif re.search("^HAS$", token):
@@ -151,13 +224,29 @@ def lexicalAnalysis():
 						ANY_OF = ANY_OF + " OF"
 						print(ANY_OF, "-Boolean Op")
 						ANY_OF = ""
-					elif ALL_OF
+					elif ALL_OF:
 						ALL_OF = ALL_OF + " OF"
 						print(ALL_OF, "-Boolean Op")
 						ALL_OF = ""
+				elif re.search("^-?[0-9]+$", token):
+					print(token,"-NUMBR")
+				elif re.search("^-?[0-9]+\.[0-9]+$",token):
+					print(token,"-NUMBAR")
+				elif re.search('^"[^"]+"$',token):
+					print(token,"-YARN")
+				elif re.search('^"[^"]*$',token):
+					YARN = YARN + token
+					isString = True
 				elif re.search("^[A-Za-z][A-Za-z0-9_]*$", token):
 					print(token, "-identifier")
-
+			elif isString:
+				if re.search('^[^"]*"$',token):
+					YARN = YARN + " " + token
+					print(YARN, "YARN")
+					YARN = ""
+					isString = False
+				else:
+					YARN = YARN + " " + token
 			elif singlelinecomment:
 				print(token, "-single-line comment word")
 				
